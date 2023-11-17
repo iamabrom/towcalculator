ad683f775adad94820b851e1dd520c60928c8a4a
Abrom <git@abrom.dev>
2023-11-16 20:03:19 -0500

    bump

--- tow-calculator/src/Calculator.tsx
+++ tow-calculator/src/Calculator.tsx
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Calculator = () => {
-  const [value1, setValue1] = useState<string>("");
-  const [value2, setValue2] = useState<string>("");
-  const [value3, setValue3] = useState<string>("");
+  // Initialize state with values from localStorage or default to empty strings
+  const [value1, setValue1] = useState<string>(
+    localStorage.getItem("value1") || ""
+  );
+  const [value2, setValue2] = useState<string>(
+    localStorage.getItem("value2") || ""
+  );
+  const [value3, setValue3] = useState<string>(
+    localStorage.getItem("value3") || ""
+  );
   const [result, setResult] = useState<number>(0);
 
+  // Update localStorage whenever the values change
+  useEffect(() => {
+    localStorage.setItem("value1", value1);
+    localStorage.setItem("value2", value2);
+    localStorage.setItem("value3", value3);
+  }, [value1, value2, value3]);
+
+  // Function to handle the calculation
   const handleCalculation = () => {
     const num1 = parseFloat(value1) || 0;
     const num2 = parseFloat(value2) || 0;
     const num3 = parseFloat(value3) || 0;
-
     const sum = num1 + num2;
     setResult(sum - num3);
   };
@@ -24,7 +38,7 @@ const Calculator = () => {
       <label>
         Value 1:
         <input
-          type="text"
+          type="number"
           value={value1}
           onChange={(e) => setValue1(e.target.value)}
         />
@@ -33,7 +47,7 @@ const Calculator = () => {
       <label>
         Value 2:
         <input
-          type="text"
+          type="number"
           value={value2}
           onChange={(e) => setValue2(e.target.value)}
         />
@@ -42,7 +56,7 @@ const Calculator = () => {
       <label>
         Subtract Value:
         <input
-          type="text"
+          type="number"
           value={value3}
           onChange={(e) => setValue3(e.target.value)}
         />
